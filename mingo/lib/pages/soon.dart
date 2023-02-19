cf1a8815ee0566ce800b7fcb56e26193c8727afd
Fatima Alaa-Elleh Smati <fatimaalaaelleh@gmail.com>
2023-02-19 22:36:15 +0100

    finished the place holder page

--- mingo/lib/pages/soon.dart
+++ mingo/lib/pages/soon.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mingo/design%20constraints/color.dart';
+import 'package:mingo/pages/homepage.dart';
 
 class SoonPage extends StatelessWidget {
   @override
@@ -6,9 +9,29 @@ class SoonPage extends StatelessWidget {
     return DecoratedBox(
       decoration: const BoxDecoration(
         image: DecorationImage(
-            image: AssetImage('images/home.jpg'), fit: BoxFit.cover),
+            image: AssetImage('images/alt.jpg'), fit: BoxFit.cover),
       ),
-      child: Column(),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 400,
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                backgroundColor: lightWidget, fixedSize: Size(250, 120)),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Homepage()),
+              );
+            },
+            child: Text(
+              "Coming soon! Stay tuned!",
+              style: GoogleFonts.playfairDisplay(fontSize: 29, color: text),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
