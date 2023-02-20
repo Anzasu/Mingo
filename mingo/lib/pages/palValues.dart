7d452c798de331d774ee74fa097f23e18a420c05
Fatima Alaa-Elleh Smati <fatimaalaaelleh@gmail.com>
2023-02-20 02:17:42 +0100

    -

---
+++ mingo/lib/pages/palValues.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mingo/design%20constraints/color.dart';
+import 'package:mingo/pages/settings.dart';
+
+class PALValuesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage('images/home.jpg'), fit: BoxFit.cover),
+      ),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            Card(
+                color: darkWidget,
+                child: Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: Text(
+                    "  PAL = Physical Activity Level \n  For regular physical activity (e.g. \n  30-60 minutes of exercise/day), you \n  can add 0.3 PAL units to your value. ",
+                    style:
+                        GoogleFonts.playfairDisplay(fontSize: 22, color: text),
+                  ),
+                )),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: lightWidget, fixedSize: Size(300, 60)),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SettingsPage()),
+                );
+              },
+              icon: Icon(
+                Icons.arrow_back,
+                size: 30.0,
+                color: text,
+              ),
+              label: Text(
+                "Back to Settings",
+                style: GoogleFonts.playfairDisplay(fontSize: 22, color: text),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Card(
+              color: darkWidget,
+              child: Padding(
+                  padding: const EdgeInsets.all(3),
+                  child: DataTable(
+                    dataRowHeight: 150,
+                    columns: [
+                      DataColumn(
+                        label: Text(
+                          "Value",
+                          style: GoogleFonts.playfairDisplay(
+                              fontSize: 22, color: text),
+                        ),
+                      ),
+                      DataColumn(
+                        label: Text(
+                          "Description",
+                          style: GoogleFonts.playfairDisplay(
+                              fontSize: 22, color: text),
+                        ),
+                      )
+                    ],
+                    rows: [
+                      DataRow(
+                        cells: <DataCell>[
+                          DataCell(Text(
+                            "1.2",
+                            style: GoogleFonts.playfairDisplay(
+                                fontSize: 22, color: text),
+                          )),
+                          DataCell(Text(
+                            "Exclusively sedentary or lying lifestyle",
+                            style: GoogleFonts.playfairDisplay(
+                                fontSize: 20, color: text),
+                          )),
+                        ],
+                      ),
+                      DataRow(
+                        cells: <DataCell>[
+                          DataCell(Text(
+                            "1.4",
+                            style: GoogleFonts.playfairDisplay(
+                                fontSize: 22, color: text),
+                          )),
+                          DataCell(Text(
+                            "Exclusively sedentary job with little or no strenuous leisure activity",
+                            style: GoogleFonts.playfairDisplay(
+                                fontSize: 20, color: text),
+                          )),
+                        ],
+                      ),
+                      DataRow(
+                        cells: <DataCell>[
+                          DataCell(Text(
+                            "1.6",
+                            style: GoogleFonts.playfairDisplay(
+                                fontSize: 22, color: text),
+                          )),
+                          DataCell(Text(
+                            "Sedentary work, at times also additional energy expenditure for walking or standing activity",
+                            style: GoogleFonts.playfairDisplay(
+                                fontSize: 20, color: text),
+                          )),
+                        ],
+                      ),
+                      DataRow(
+                        cells: <DataCell>[
+                          DataCell(Text(
+                            "1.8",
+                            style: GoogleFonts.playfairDisplay(
+                                fontSize: 22, color: text),
+                          )),
+                          DataCell(Text(
+                            "Predominantly walking and standing work",
+                            style: GoogleFonts.playfairDisplay(
+                                fontSize: 20, color: text),
+                          )),
+                        ],
+                      ),
+                      DataRow(
+                        cells: <DataCell>[
+                          DataCell(Text(
+                            "2.0",
+                            style: GoogleFonts.playfairDisplay(
+                                fontSize: 22, color: text),
+                          )),
+                          DataCell(Text(
+                            "Physically demanding professional work",
+                            style: GoogleFonts.playfairDisplay(
+                                fontSize: 20, color: text),
+                          )),
+                        ],
+                      ),
+                    ],
+                  )),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- mingo/lib/pages/settings.dart
+++ mingo/lib/pages/settings.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mingo/design%20constraints/color.dart';
+import 'package:mingo/pages/palValues.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
@@ -8,14 +11,274 @@ class SettingsPage extends StatefulWidget {
 }
 
 class SettingsState extends State<SettingsPage> {
+  //temporary storage of user input values. The variables will be set to a random value
+  double _weight = 90.0;
+  String _gender = "Female";
+  int _age = 20;
+  double _palValue = 0;
+  String _goal = "Maintain weight";
+
   @override
   Widget build(BuildContext context) {
     return DecoratedBox(
       decoration: const BoxDecoration(
         image: DecorationImage(
-            image: AssetImage('images/home.jpg'), fit: BoxFit.cover),
+            image: AssetImage('images/alt.jpg'), fit: BoxFit.cover),
       ),
-      child: Column(),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 140,
+            ),
+            Card(
+              color: darkWidget,
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Text(
+                  "Settings",
+                  style: GoogleFonts.playfairDisplay(fontSize: 29, color: text),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 20,
+                ),
+                Card(
+                  color: lightWidget,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      "Weight in kg \n(Choose the value closest to your weight))",
+                      style: GoogleFonts.playfairDisplay(
+                          fontSize: 22, color: text),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 20,
+                ),
+                Card(
+                  color: lightWidget,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      "Age (in years)",
+                      style: GoogleFonts.playfairDisplay(
+                          fontSize: 22, color: text),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: DropdownButton<int>(
+                  value: _age,
+                  //elevation: 5,
+                  iconEnabledColor: altText,
+                  items: <int>[
+                    13,
+                    14,
+                    15,
+                    16,
+                    17,
+                    18,
+                    19,
+                    20,
+                    21,
+                    22,
+                    23,
+                    24,
+                    25,
+                    26,
+                    27,
+                    28,
+                    29,
+                    30,
+                    31,
+                    32,
+                    33,
+                    34,
+                    35,
+                    36,
+                    37,
+                    38,
+                    39,
+                    40,
+                    41,
+                    42,
+                    43,
+                    44,
+                    45,
+                    46,
+                    47,
+                    48,
+                    49,
+                    50,
+                    51,
+                    52,
+                    53,
+                    54,
+                    55,
+                    56,
+                    57,
+                    58,
+                    59,
+                    60,
+                    61,
+                    62,
+                    63,
+                    64,
+                    65,
+                    66,
+                    67,
+                    68,
+                    69,
+                    70,
+                  ].map<DropdownMenuItem<int>>((int value) {
+                    return DropdownMenuItem<int>(
+                      value: value,
+                      child: Text(
+                        value.toString(),
+                        style: GoogleFonts.playfairDisplay(
+                            fontSize: 22, color: altText),
+                      ),
+                    );
+                  }).toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      _age = value!;
+                    });
+                  },
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 20,
+                ),
+                Card(
+                  color: lightWidget,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      "Gender",
+                      style: GoogleFonts.playfairDisplay(
+                          fontSize: 22, color: text),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: DropdownButton<String>(
+                  value: _gender,
+                  //elevation: 5,
+                  iconEnabledColor: altText,
+
+                  items: <String>[
+                    'Male',
+                    'Female',
+                  ].map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(
+                        value,
+                        style: GoogleFonts.playfairDisplay(
+                            fontSize: 22, color: altText),
+                      ),
+                    );
+                  }).toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      _gender = value!;
+                    });
+                  },
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 24,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: lightWidget, fixedSize: Size(300, 60)),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => PALValuesPage()),
+                    );
+                  },
+                  child: Text(
+                    "PAL value (Click to determine suitable value)",
+                    style:
+                        GoogleFonts.playfairDisplay(fontSize: 22, color: text),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 20,
+                ),
+                Card(
+                  color: lightWidget,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      "Goal",
+                      style: GoogleFonts.playfairDisplay(
+                          fontSize: 22, color: text),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 45,
+                ),
+                ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: darkWidget, fixedSize: Size(300, 80)),
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.save,
+                    size: 30.0,
+                    color: text,
+                  ),
+                  label: Text(
+                    "Save and go back",
+                    style:
+                        GoogleFonts.playfairDisplay(fontSize: 22, color: text),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
